d52d071e6aade6287a955b6e3a3b8f05d4a1d33b
Umesh Oad <umesh.oad@ucalgary.ca>
2023-08-24 16:30:21 -0600

    Finished First and Second Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
 
+let str;
+let num;
+const repeatString = function (str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        return str = str.repeat(num);
+      
+    }
 };
 
 // Do not edit below this line
